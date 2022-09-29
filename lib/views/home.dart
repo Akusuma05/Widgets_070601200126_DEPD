6dabf792c4dc6e056841a41dbf82aecaddf7b143
Angelo Kusuma <84068505+Akusuma05@users.noreply.github.com>
2022-09-29 20:33:59 +0700

    Border Radius, Benerin button like

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -8,19 +8,21 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool click = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Widgeting"),
+        title: Text("Saekano"),
         centerTitle: true,
       ),
       body: Container(
         decoration: BoxDecoration(
             gradient: LinearGradient(
-                begin: Alignment.topRight,
-                end: Alignment.bottomLeft,
-                colors: [Colors.blue, Colors.red])),
+                begin: Alignment.centerRight,
+                end: Alignment.centerLeft,
+                colors: [Colors.white, Colors.blue])),
         width: double.infinity,
         height: double.infinity,
         child: Column(children: [
@@ -40,13 +42,18 @@ class _HomeState extends State<Home> {
                   Align(
                     alignment: Alignment(0.9, -0.8),
                     child: ElevatedButton.icon(
-                      onPressed: () {},
-                      icon: Icon(Icons.heart_broken, color: Colors.red),
+                      onPressed: () {
+                        setState(() {
+                          click = !click;
+                        });
+                      },
+                      icon: Icon(Icons.favorite,
+                          color: (click == false) ? Colors.red : Colors.grey),
                       label: Text(''),
                       style: ElevatedButton.styleFrom(
                           shadowColor: Colors.white,
                           elevation: 0,
-                          padding: const EdgeInsets.all(8),
+                          padding: const EdgeInsets.all(15),
                           minimumSize: const Size.fromRadius(12),
                           primary: Colors.white,
                           shape: RoundedRectangleBorder(
@@ -62,14 +69,46 @@ class _HomeState extends State<Home> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Image.asset('assets/images/profile1.jpg',
-                      width: 90, height: 120),
-                  Image.asset('assets/images/profile1.jpg',
-                      width: 90, height: 120),
-                  Image.asset('assets/images/profile1.jpg',
-                      width: 90, height: 120),
-                  Image.asset('assets/images/profile1.jpg',
-                      width: 90, height: 120)
+                  Container(
+                    padding: EdgeInsets.all(8),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: SizedBox.fromSize(
+                          size: Size.fromRadius(40),
+                          child: Image.asset('assets/images/1.jpg',
+                              fit: BoxFit.cover),
+                        )),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(8),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: SizedBox.fromSize(
+                          size: Size.fromRadius(40),
+                          child: Image.asset('assets/images/2.jpg',
+                              fit: BoxFit.cover),
+                        )),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(8),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: SizedBox.fromSize(
+                          size: Size.fromRadius(40),
+                          child: Image.asset('assets/images/3.jpg',
+                              fit: BoxFit.cover),
+                        )),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(8),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: SizedBox.fromSize(
+                          size: Size.fromRadius(40),
+                          child: Image.asset('assets/images/4.jpg',
+                              fit: BoxFit.cover),
+                        )),
+                  ),
                 ],
               )),
           Flexible(
@@ -80,14 +119,14 @@ class _HomeState extends State<Home> {
                 Container(
                   child: const Center(
                       child: Text(
-                    'Test',
+                    'Synopsis',
                     style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                   )),
                 ),
                 Container(
                   child: const Center(
                       child: Text(
-                          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.')),
+                          'Tomoya Aki, an otaku, has been obsessed with collecting anime and light novels for years, attaching himself to various series with captivating stories and characters. Now, he wants to have a chance of providing the same experience for others by creating his own game, but unfortunately, Tomoya cannot do this task by himself. He successfully recruits childhood friend Eriri Spencer Sawamura to illustrate and literary elitist Utaha Kasumigaoka to write the script for his visual novel, while he directs. Super-group now in hand, Tomoya only needs an inspiration to base his project on, and luckily meets the beautiful, docile Megumi Katou, who he then models his main character after. Using what knowledge he has, Tomoya creates a new doujin circle with hopes to touch the hearts of those who play their game. What he does not realize, is that to invoke these emotions, the creators have had to experience the same feelings in their own lives.')),
                 ),
                 Container(
                   child: const Center(
